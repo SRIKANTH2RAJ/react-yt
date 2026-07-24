039a4079327fc6e65508899947e9531e776a3881
SRIKANTH2RAJ <srikanth16raj@gmail.com>
2026-07-24 15:55:11 +0530

    my-vite-project done

---
+++ my-vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="Parent">
+      <h1 id="child1">App</h1>
+      <h2 id="child2">Welcome to Vite!</h2>
+    </div>
+    <div id="chacha"></div>
+    </>
+  )
+}
+
+export default App
+   
