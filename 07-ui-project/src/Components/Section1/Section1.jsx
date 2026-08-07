aafd194e637e4d8e329a1b98c61ae196d41990c3
SRIKANTH2RAJ <srikanth16raj@gmail.com>
2026-08-07 19:42:56 +0530

    Started with a new project

---
+++ 07-ui-project/src/Components/Section1/Section1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar.jsx'   
+
+const Section1 = () => {
+  return (
+    <div className="h-screen w-full bg-white">
+      <Navbar />
+      
+    </div>
+  )
+} 
+
+export default Section1
