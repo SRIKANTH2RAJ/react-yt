f71320db3e72406bbbaf75398912265a7e71c5c0
SRIKANTH2RAJ <srikanth16raj@gmail.com>
2026-08-20 19:07:13 +0530

    Explored on React-functions

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const App = () => {
+  function btnclick(){
+    console.log("Button clicked")
+  }
+  function change(val){
+    console.log(val)
+  }
+  function scroll_speed(val){
+    console.log(val)
+  }
+  return (
+    <div onWheel={(elem)=>{
+      scroll_speed(elem)
+    }}>
+      <div className='page1'>
+      <h1>hi hello how are you how is it</h1>
+      <button onClick={()=> {
+        console.log("button is clicked")
+      }}
+      onDoubleClick={()=>{
+        console.log("button is double clicked")
+      }}>Click Here</button>
+      <br />
+      <input id="i" type="text" placeholder='Enter Name' onChange={()=>{
+        console.log("The user is typing")
+      }} />
+      <input  id="iu" type="text" placeholder='Passion' onChange={(elem)=>{
+          change(elem.target.value)
+      }} />
+      
+    </div>
+    <div className='page2'></div>
+    <div className='page3'></div>
+    </div>
+  )
+}
+
+export default App
